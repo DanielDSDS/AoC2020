3df28d44ceee43c7e59de97be20a4b76df5b6fb4
nedBigby <bigbigbyned@gmail.com>
2020-12-03 02:43:23 -0400

    [Day 1] - Add solution to day 1 part 2

--- AoC2020.js
+++ AoC2020.js
@@ -12,9 +12,20 @@ function read(file, cb) {
   });
 }
 
-
 //Day 1: Sum up to 2020, then multiply
-//First try: brute force
+
+//obtain input array from text file and run solution
+read(__dirname+'/01/input.txt', function(data) {
+    var input = [];
+    for(var item in data){
+        input.push(+data[item].match(/\d+/g));
+    }
+   
+    console.log("Day 1 part 1:",elfsAccounting(input))
+    console.log("Day 1 part 2:",elfsAccounting2(input))
+});
+
+//Part 1 first try: brute force
 //Time complexity: O(n^2)
 function elfsAccounting(inputArr) {
     for( var i = 0; i < inputArr.length; i++){
@@ -26,17 +37,17 @@ function elfsAccounting(inputArr) {
     } 
 }
 
-//obtain input array from text file
-
-read(__dirname+ '/01/input.txt', function(data) {
-  var input = [];
-  for(var num in data){
-    input.push(+data[num].match(/\d+/g));
-  }
-});
-
-function elfsAccounting2(inputArr2){
-    
+//Part 2 first try: brute force also
+//Time complexity O(n^3)
+function elfsAccounting2(inputArr) {
+    for( var i = 0; i < inputArr.length; i++){
+        for( var j = 0; j < inputArr.length; j++){
+            for( var k = 0; k < inputArr.length; k++){
+                var sum = inputArr[i] + inputArr[j] + inputArr[k]
+                if(sum === 2020 && inputArr[i] && inputArr[j] &&inputArr[k]) 
+                    return inputArr[i]*inputArr[j]*inputArr[k]
+            }
+        }
+    } 
 }
 
-console.log(elfsAccounting(inputArr));
