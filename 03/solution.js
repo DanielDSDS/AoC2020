16bb78c6f02d819d00c740a6b3d671e4ebc7d197
nedBigby <bigbigbyned@gmail.com>
2020-12-07 23:02:51 -0400

    Add Day 3 P2 solution

--- 03/solution.js
+++ 03/solution.js
@@ -22,9 +22,11 @@ read(__dirname+'/input.txt', function(data) {
         input.push(data[item].match(/[.#]/g));
     }
 
-    var t = trees(input)
-    console.log('trees: ',t)
-    console.log('1,3',input[1][3],'2,6',input[1][6])
+    var slopes = [[1,1],[3,1],[5,1],[7,1],[1,2]]
+    var t = trees(input,slopes,1)
+    var s = p2(input,slopes)
+    console.log('p1 trees: ',t)
+    console.log('p2 ans: ',s)
     return input;
 });
 
@@ -32,18 +34,29 @@ read(__dirname+'/input.txt', function(data) {
 //Day 3: obstructed path
 //Time complexity: prob O(n)
 
-function trees(a){
+function trees(a,sl,ix){
     var t = 0;
-    for(var i = 1, j = 0; i < a.length - 1; i++){
-        j = j + 3 
+    var inc1 = sl[ix][0];
+    var inc2 = sl[ix][1];
+    for(var i = inc2, j = 0; i < a.length - 1; i += inc2){
+        j = j + inc1 
             if(a[i][j%a[i].length] === '#'){
                 console.log('i: ',i,'j: ',j,'X')
                 t++ 
             }else{
-                console.log('i: ',i,'j: ',j)
+                //console.log('i: ',i,'j: ',j)
             }
     }
     return t;
+}
+
+function p2(i,s){
+    var ans = 1;
+    s.forEach((sl,k)=>{
+        ans *= trees(i,s,k) 
+    })
+
+    return ans
 }
 
 
