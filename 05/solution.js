318486410567a2884217b65382b7c46b8c0588bf
nedBigby <bigbigbyned@gmail.com>
2020-12-13 23:01:12 -0400

    Data Mapping for Day 5 part 1 solution

---
+++ 05/solution.js
@@ -0,0 +1,28 @@
+var fs = require("fs");
+var input = [];
+
+
+//defining the read input file function
+function read(file, cb) {
+    var readInput = []; 
+    fs.readFile(file, 'utf8', function(err, data) {
+        if (!err) {
+            readInput = cb(data.toString().split('\n'))
+        } else {
+            console.log(err)
+        }
+    });
+
+}
+
+//obtain input array from text file and run solution
+read(__dirname+'/input.txt', function(data) {
+    var input = [];
+    for(var item in data){
+        input.push(data[item])
+        input[item] = input[item].replace(/\r/g,'') 
+    }
+    console.log(input)
+
+    return input;
+});
