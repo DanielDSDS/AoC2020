dd9dd34b59190b790455e04e86b0034fffb8b2cf
Daniel Di Mella <bigbigbyned@gmail.com>
2020-12-23 16:02:15 -0400

    Day 5 P1 solution

--- 05/solution.js
+++ 05/solution.js
@@ -22,7 +22,64 @@ read(__dirname+'/input.txt', function(data) {
         input.push(data[item])
         input[item] = input[item].replace(/\r/g,'') 
     }
-    console.log(input)
+		
+		console.log(calculateSeat(input))
 
     return input;
 });
+
+const calculateSeat = (seats) => {
+	let max = 0
+	for(var line in seats){	
+		let ID = calculateID(seats[line])	
+			if(ID > max){
+				console.log(seats[line])	
+				max = ID
+			}
+	}
+	return max
+}
+
+const calculateID = (ticket) => {
+	let arr = ticket.split("")
+	let start = 0
+	let ref = 127
+	let colStart = 0
+	let colRef = 7
+	let a1 = 0
+	let a2 = 0
+	for(let i=0; i<10; i++){
+		let delta = ref - start 
+		let colDelta = colRef-colStart  
+		if(i<7)
+			if(arr[i] === 'F' ){	
+				ref = Math.trunc(ref - delta/2)
+				a1 = ref
+			}else{
+				start = Math.trunc(start + delta/2)
+				a1 = start 
+			}
+		else
+			if(arr[i] === 'L' ){	
+				colRef = Math.trunc(colRef - colDelta/2)
+				a2 = colRef
+			}else{
+				colStart = Math.trunc(colStart + colDelta/2)
+				a2 = colStart+1 
+			}
+		//	console.log('[',arr[i],'] pos',i,' s: ',start,' r:', ref)
+	}
+
+	return a1*8+a2 
+
+}
+
+
+
+
+
+
+
+
+
+
